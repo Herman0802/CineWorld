32ae541c2e14a12bc33e180a208cad6f81eade24
Herman0802 <germanlazar02@gmail.com>
2024-06-15 23:20:51 +0200

    Initial commit

---
+++ src/main/java/org/example/cineworld/CineWorldApplication.java
@@ -0,0 +1,13 @@
+package org.example.cineworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineWorldApplication.class, args);
+    }
+
+}
